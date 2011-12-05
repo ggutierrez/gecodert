3a6afc60aa9e3419c86c6ba997ee4daa76e18fd9
Gustavo A. Gómez Farhat <gustavo.gomez.farhat@gmail.com>
2011-12-05 16:05:40 -0500

    More integer rel propagators implemented.

--- gecodert/int/rel.cpp
+++ gecodert/int/rel.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 #include <gecodert/int/rel.hh>
+#include <gecodert/int.hh>
 
 using namespace std;
 
 namespace GecodeRT {
-  
-  void rel(GecodeSpace& home, CtVar x0, IntRelType r, int c){
+
+  void rel(GecodeSpace& home, CtVar x0, IntRelType r, CtVar x1) {
+    Gecode::rel(home, home.intVar(x0), static_cast<Gecode::IntRelType>(r), home.intVar(x1));
+  }
+
+  void rel(GecodeSpace& home, const std::vector<CtVar>& x, IntRelType r, CtVar y) {
+    Gecode::IntVarArgs iv;
+    for(const CtVar& cv : x){
+      iv << home.intVar(cv);
+    }
+    Gecode::rel(home, iv, static_cast<Gecode::IntRelType>(r), home.intVar(y));
+  }
+
+  void rel(GecodeSpace& home, CtVar x0, IntRelType r, int c) {
     Gecode::rel(home, home.intVar(x0), static_cast<Gecode::IntRelType>(r), c);
   }
+  
+  void rel(GecodeSpace& home, const std::vector<CtVar>& x, IntRelType r) {
+    Gecode::IntVarArgs iv;
+    for(const CtVar& cv : x){
+      iv << home.intVar(cv);
+    }
+    Gecode::rel(home, iv, static_cast<Gecode::IntRelType>(r));
+  }
+  
   
 }
--- gecodert/int/rel.hh
+++ gecodert/int/rel.hh
@@ -5,9 +5,19 @@
 #include <gecodert/int.hh>
 
 namespace GecodeRT {
-  
-  /// Propagates $ x \sim_r c$. 
+
+  /// Post propagator for $ x_0 \sim_r x_1$.
+  void rel(GecodeSpace& home, CtVar x0, IntRelType r, CtVar x1);
+
+  /// Post propagator for $ x_i \sim_r y $ for all $0\leq i<|x|$. 
+  void rel(GecodeSpace& home, const std::vector<CtVar>& x, IntRelType r, CtVar y);
+
+  /// Propagates $ x \sim_r c$.
   void rel(GecodeSpace& home, CtVar x0, IntRelType r, int c);
+
+  /// Propagates $ x_i \sim_r c $ for all $0\leq i<|x|$. 
+  void rel(GecodeSpace& home, const std::vector<CtVar>& x, IntRelType r);
+
   
 }
 
