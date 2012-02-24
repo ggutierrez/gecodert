d18e088444bc6f060e96f05d48f066b89f168929
Gustavo A. Gómez Farhat <gustavo.gomez.farhat@gmail.com>
2012-02-24 15:41:15 -0500

    Added some linear propagators.

--- gecodert/int/linear.cpp
+++ gecodert/int/linear.cpp
@@ -1,11 +1,24 @@
+
 #include <gecodert/int/linear.hh>
 
 using namespace std;
 
 namespace GecodeRT {
-  
-  void linear(GecodeSpace& home, const std::vector<int>& a, const std::vector<CtVar>& x, IntRelType r, int c) {
+
+  void linear(GecodeSpace& home, const std::vector<CtVar>& x, IntRelType r, int c){
+    Gecode::linear(home, home.toIntVarArgs(x), static_cast<Gecode::IntRelType>(r), c);
+  }
+
+  void linear(GecodeSpace& home, const std::vector<CtVar>& x, IntRelType r, CtVar y){
+    Gecode::linear(home, home.toIntVarArgs(x), static_cast<Gecode::IntRelType>(r), home.intVar(y));
+  }
+
+  void linear(GecodeSpace& home, const std::vector<int>& a, const std::vector<CtVar>& x, IntRelType r, int c){
     Gecode::linear(home, Gecode::IntArgs(a), home.toIntVarArgs(x), static_cast<Gecode::IntRelType>(r), c);
   }
-  
+
+  void linear(GecodeSpace& home, const std::vector<int>& a, const std::vector<CtVar>& x, IntRelType r, CtVar y){
+    Gecode::linear(home, Gecode::IntArgs(a), home.toIntVarArgs(x), static_cast<Gecode::IntRelType>(r), home.intVar(y));
+  }
+
 }
--- gecodert/int/linear.hh
+++ gecodert/int/linear.hh
@@ -6,9 +6,11 @@
 
 namespace GecodeRT {
 
-  /// Post propagator for $\sum_{i=0}^{|x|-1}a_i\cdot x_i\sim_r c$. 
+  void linear(GecodeSpace& home, const std::vector<CtVar>& x, IntRelType r, int c);
+  void linear(GecodeSpace& home, const std::vector<CtVar>& x, IntRelType r, CtVar y);
   void linear(GecodeSpace& home, const std::vector<int>& a, const std::vector<CtVar>& x, IntRelType r, int c);
-      
+  void linear(GecodeSpace& home, const std::vector<int>& a, const std::vector<CtVar>& x, IntRelType r, CtVar y);
+
 }
 
 #endif
